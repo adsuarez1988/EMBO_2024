c9ddeb48ce37b6d7ffa3c3b3d5bb13084a54abb2
adsuarez <alfonso.suarez@emu.ee>
2024-06-12 16:41:09 +0200

    Day3 afternoon

--- Day3.R
+++ Day3.R
@@ -110,6 +110,63 @@ postMean <- B*mu + (1-B)*y
 postVar <- B*tau^2
 points(x=x, y=dnorm(x=x, mean=postMean, sd=sqrt(postVar)), type="l", lty=3)
 
- ############## PRACTICAL 1 #############
+ ############## Afternoon #############
 
+
+install.packages("abc")
+library(abc)
+
+sims <- read.csv("mosquito-task2.csv", head=T)
+
+
+# check prior distributions
+x11()
+par(mfrow=c(2,2))
+hist(sims$N1)
+hist(sims$N2)
+hist(sims$T_split)
+hist(sims$MigRate)
+
+# remove simulations with NaN for some summary stats!
+
+# find useful summary stats which correlate with T_split
+cor(sims$Fst, sims$T_split)
+cor(sims$dxy, sims$T_split)
+cor(sims$segsites1, sims$T_split)
+cor(sims$segsites2, sims$T_split)
+cor(sims$pi1, sims$T_split)
+cor(sims$pi2, sims$T_split)
+cor(sims$tajima1, sims$T_split)
+cor(sims$tajima2, sims$T_split)
+
+obs <- read.csv("mosquito-observed.csv", head=T)
+
+# check if simulated retained summary stats contain the observed one
+quantile(sims$Fst); cat(obs$Fst)
+quantile(sims$segsites1); cat(obs$segsites1)
+quantile(sims$segsites2); cat(obs$segsites2)
+
+# merge obs with retained sims to scale them
+sumstats <- scale(rbind(obs[c(1,3,4)],sims[,c(5,7,8)]))
+
+est <- abc(target=sumstats[1,], param=sims$T_split, sumstat=sumstats[-1,], tol=0.05, method="rejection")
+
+hist(est$dist)
+abline(v=max(est$dist[which(est$region)]), lty=2)
+
+# posterior
+x11()
+par(mfrow=c(2,1))
+hist(est$unadj.values, freq=FALSE, xlim=range(sims$T_split), col=rgb(0,0,1,1/4), main="Posterior probability", xlab="Split time")
+
+# MAP
+map <- mean(est$unadj.values)
+abline(v=map, lty=2)
+
+# confidence intervals
+hpd <- quantile(x=est$unadj.values, probs=c(0.025,0.975))
+abline(v=hpd, lty=3)
+
+# prior
+hist(sims$T_split, freq=FALSE, xlim=range(sims$T_split), col=rgb(1,0,0,1/4))
 
